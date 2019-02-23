627fe84fcc93476004ba74c75ca1dd25707cd62a
Austin Walhof <ajwgeek@gmail.com>
2019-02-22 22:02:40 -0500

    Cleanup Landing Page

--- page-landing.php
+++ page-landing.php
@@ -4,21 +4,29 @@
  * Template Post Type: page
  */
 
-define('GOOGLE_CALENDAR_ID', 'mtu.edu_09qi6pfnktib7heh2grshjvqo4@group.calendar.google.com');
-
-$context = Timber::get_context();
+$NUM_EVENTS = 4;
+$GOOGLE_PERMISSIONS = Google_Service_Calendar::CALENDAR_READONLY;
+$GOOGLE_AUTH_CONFIG = '/credentials.json';
+$GOOGLE_CALENDAR_ID = 'mtu.edu_09qi6pfnktib7heh2grshjvqo4@group.calendar.google.com';
 
 $client = new Google_Client();
-$client->setApplicationName('Valhalla');
-$client->setScopes(Google_Service_Calendar::CALENDAR_READONLY);
-$client->setAuthConfig('credentials.json');
+$client->setAuthConfig(get_template_directory() . $GOOGLE_AUTH_CONFIG);
+$client->setScopes($GOOGLE_PERMISSIONS);
+
+$options = array(
+	'maxResults'   => $NUM_EVENTS, 
+	'orderBy'      => 'startTime', 
+	'singleEvents' => true, 
+	'timeMin'      => date('c')
+);
 
 $service = new Google_Service_Calendar($client);
-$options = array('maxResults' => 4, 'orderBy' => 'startTime', 'singleEvents' => true, 'timeMin' => date('c'));
-$results = $service->events->listEvents(GOOGLE_CALENDAR_ID, $options);
+$results = $service->events->listEvents($GOOGLE_CALENDAR_ID, $options);
 $results = $results->getItems();
 
+$context = Timber::get_context();
 $context['calendar_events'] = $results;
 
 Timber::render('pages/landingPage/LandingPage.twig', $context);
 ?>
+
